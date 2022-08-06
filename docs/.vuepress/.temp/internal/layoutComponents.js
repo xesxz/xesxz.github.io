7917efddd6935d0ec5632f4f535d8c303ad1db62
xesxz <leeycheung@gmail.com>
2022-08-07 00:08:01 +0800

    update .gitignore

--- docs/.vuepress/.temp/internal/layoutComponents.js
+++
@@ -1,6 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const layoutComponents = {
-  "404": defineAsyncComponent(() => import("/Users/wk/xesxz.github.io/docs/.vuepress/theme/layouts/404.vue")),
-  "Layout": defineAsyncComponent(() => import("/Users/wk/xesxz.github.io/node_modules/@vuepress/theme-default/lib/client/layouts/Layout.vue")),
-}
