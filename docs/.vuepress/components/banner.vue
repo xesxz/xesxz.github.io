814fbd529d706b1330403a3847a772e81bc0cb85
xesxz <leeycheung@gmail.com>
2022-08-11 02:44:18 +0800

    a'sd

--- docs/.vuepress/components/banner.vue
+++ docs/.vuepress/components/banner.vue
@@ -17,7 +17,7 @@ export default {
   },
   methods:{
     getWallpaper(){
-      fetch('http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1').then(res => {
+      fetch('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1').then(res => {
         return res.json()
       }).then(data => {
         this.imgUrl =  'https://cn.bing.com/' + data.images[0].url
