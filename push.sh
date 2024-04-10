ad9cb968aefa538eb691bb1c0a2b832ca520b0dd
张磊 <zhanglei@cdgis.cc>
2024-04-10 16:30:14 +0800

    Auto commit by shell script

--- push.sh
+++ push.sh
@@ -8,6 +8,10 @@ cd /Users/wk/xesxz.github.io
     # 执行git命令
     git add --all && git commit -m "$fix_message" && git push
 
+    这里如果出现 Enter passphrase for key '/Users/wk/.ssh/id_rsa':
+    自动填入密码5461150
+
+
 
     # 输出结果
     echo "Auto commit by shell script"
