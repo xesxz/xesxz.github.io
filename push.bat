af110051ae6093538ee791b54002f41d370bb05b
xesxz <leeycheung@gmail.com>
2024-07-31 16:17:29 +0800

    Auto commit by shell script

---
+++ push.bat
@@ -0,0 +1,12 @@
+@echo off
+
+
+set fix_message="Auto commit by shell script"
+
+:: Execute git commands
+git add --all
+git commit -m %fix_message%
+git push
+
+:: Output result
+echo Auto commit by shell script
